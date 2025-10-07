4255f8d59f3b4bb188aa440735bcf9d55b3af0a5
ParcheVerde <guevararamirez121@gmail.com>
2025-10-07 16:17:37 -0600

    CRUD Finalizado v1
    Hace falta agregar la carga de imagenes pero en temas de datos planos todo esta bien
    
    auto-ptt: Revisar detalladamente con tiempo luego para asimilar todo.

--- ReservaYa/App_Start/RouteConfig.cs
+++ ReservaYa/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ReservaYa
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "GestionEspacios", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
