e1c5f6ac83a6b44b28da410812fda40cb8d576db
Fabio <rm712@proton.me>
2025-10-11 21:40:03 -0600

    Updated links and fixed image heights

--- ReservaYa/App_Start/RouteConfig.cs
+++ ReservaYa/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ReservaYa
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "GestionEspacios", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Login", action = "Register", id = UrlParameter.Optional }
             );
 
             //routes.MapRoute(
